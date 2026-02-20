819a71171c5aaccd65ee5cfdb7051cd0426a6cc0
Abhilash M <abhilashm@ivoyant.com>
2026-02-20 13:39:26 +0530

    updated for New config

---
+++ src/main/java/com/dataflow/dataloaders/entity/ConfigSchema.java
@@ -0,0 +1,16 @@
+package com.dataflow.dataloaders.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConfigSchema {
+    private List<ConfigSection> sections;
+}
--- src/main/java/com/dataflow/dataloaders/entity/Provider.java
+++ src/main/java/com/dataflow/dataloaders/entity/Provider.java
@@ -1,6 +1,5 @@
 package com.dataflow.dataloaders.entity;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +16,6 @@ public class Provider extends AuditMetaData {
     private String description;
     private String iconId;
     private Integer defaultPort;
-    private JsonNode configSchema;
+    private ConfigSchema configSchema;
     private Integer displayOrder;
 }
